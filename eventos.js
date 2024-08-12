1c55dfbf4e6a338bfc20e0c5e985363e70b5226c
Brian <briancamilaa@gmail.com>
2024-08-12 11:56:08 -0300

    Agregado del div con su alerta

---
+++ eventos.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('div').addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
